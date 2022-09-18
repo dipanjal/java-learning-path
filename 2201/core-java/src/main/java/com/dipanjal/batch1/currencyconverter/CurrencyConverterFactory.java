86ea2d98e73c3398f43fc334a26683f3e471f4aa
dipanjal <dipanjalmaitra@gmail.com>
2022-09-18 13:32:58 +0600

    factory modified with concurrent-hashmap

--- 2201/core-java/src/main/java/com/dipanjal/batch1/currencyconverter/CurrencyConverterFactory.java
+++ 2201/core-java/src/main/java/com/dipanjal/batch1/currencyconverter/CurrencyConverterFactory.java
@@ -1,15 +1,28 @@
 package com.dipanjal.batch1.currencyconverter;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 public class CurrencyConverterFactory {
 
+    private ConcurrentHashMap<String, CurrencyConverter> ioc = new ConcurrentHashMap<>();
+
+    private CurrencyConverterFactory() {
+        //bean registration
+        ioc.put("USD", new USDCurrencyConverter());
+        ioc.put("INR", new INRCurrencyConverter());
+        ioc.put("EUR", new EURCurrencyConverter());
+    }
+
     public CurrencyConverter getConverter(String type){
-        if(type.equals("USD")) {
-            return new USDCurrencyConverter();
-        } else if (type.equals("INR")) {
-            return new INRCurrencyConverter();
-        } else if (type.equals("EUR")) {
-            return new EURCurrencyConverter();
-        }
-        return null;
+//        if(type.equals("USD")) {
+//            return new USDCurrencyConverter();
+//        } else if (type.equals("INR")) {
+//            return new INRCurrencyConverter();
+//        } else if (type.equals("EUR")) {
+//            return new EURCurrencyConverter();
+//        }
+//        return null;
+
+        return ioc.getOrDefault(type, null);
     }
 }
